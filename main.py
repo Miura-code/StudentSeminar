6d24af225627885107a620077dc5cab8faccc39e
miura-Miura <smiuymiu0official0@gmail.com>
2024-10-29 15:25:52 +0900

    update:

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import os
+import numpy as np
 from config.main_config import MainConfig
 from utils.logging import get_std_logging
 
+from torch.utils.tensorboard import SummaryWriter
 
 def main():
     config.print_params(logger.info)
     logger.info("-> ログを開始")
     logger.debug("-> デバックのためのログ")
+
+    writer = SummaryWriter(log_dir=os.path.join(config.path, "tb"))
+    writer.add_text('config', config.as_markdown(), 0)
+    
+    data = np.linspace(0, 100, 300)
+    for epoch, d in enumerate(data):
+        writer.add_scalar('train/data', d, epoch)
+    writer.close()
+
 
 if __name__ == "__main__":
     config = MainConfig()
